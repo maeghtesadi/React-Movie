efb4ea6512a040a49956973c0dd6654d4059d015
Ali <ma_eghtesadi@yahoo.com>
2025-04-07 13:02:24 -0400

    Project Base commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "9cace7d0934bb8b43587c3e3ba31f3bf";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
